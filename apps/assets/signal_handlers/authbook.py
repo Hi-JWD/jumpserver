6c7ce9d4c11c62d4ee92ab156b817f6cba071ff6
jiangweidong <weidong.jiang@fit2cloud.com>
2022-07-04 18:47:23 +0800

    perf: 优化更改账户是否可改密不会更新版本信息

--- apps/assets/signal_handlers/authbook.py
+++ apps/assets/signal_handlers/authbook.py
@@ -1,7 +1,7 @@
 from django.dispatch import receiver
 from django.apps import apps
 from simple_history.signals import pre_create_historical_record
-from django.db.models.signals import post_save, pre_save, pre_delete
+from django.db.models.signals import post_save, pre_save, pre_delete, post_init
 
 from common.utils import get_logger
 from ..models import AuthBook, SystemUser
@@ -43,9 +43,25 @@ def on_authbook_post_create(sender, instance, created, **kwargs):
         # instance.update_asset_admin_user_if_need()
 
 
+@receiver(post_init, sender=AuthBook)
+def on_authbook_init_create(sender, instance, **kwargs):
+    contrastive_attrs = ['allow_change_auth']
+    for attr in contrastive_attrs:
+        ori_attr = getattr(instance, attr)
+        setattr(instance, '__original_%s' % attr, ori_attr)
+
+
 @receiver(pre_save, sender=AuthBook)
 def on_authbook_pre_create(sender, instance, **kwargs):
-    # 升级版本号
-    instance.version += 1
+    contrastive_attrs = ['allow_change_auth']
+    change_version = True
+    for attr in contrastive_attrs:
+        current_attr = getattr(instance, attr)
+        ori_attr = getattr(instance, '__original_%s' % attr)
+        if current_attr != ori_attr:
+            change_version = False
+    if change_version:
+        # 升级版本号
+        instance.version += 1
     # 即使在 root 组织也不怕
     instance.org_id = instance.asset.org_id
