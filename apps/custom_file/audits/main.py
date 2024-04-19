b8f508d9566ec201a09c5319ba3b25270297e0b2
jiangweidong <1053570670@qq.com>
2024-04-19 10:12:45 +0800

    perf: 日志回调函数优化

--- apps/custom_file/audits/main.py
+++ apps/custom_file/audits/main.py
@@ -1,4 +1,6 @@
 # data/audits/main.py
+import os
+import time
 
 import requests
 
@@ -11,15 +13,23 @@ from common.utils import get_logger
 logger = get_logger(__name__)
 
 
+os.makedirs(os.path.join(settings.DATA_DIR, 'audits'), exist_ok=True)
+FAILED_REQUEST_FILE = os.path.join(settings.DATA_DIR, 'audits', 'audits_callback_failed.txt')
+
+
 def audit_callback(category, data):
     if category == 'host_session_log' and settings.AUDIT_CALLBACK_URL:
-        try:
-            data['job_id'] = JobUtil(data['user_id']).get_job()
-            resp = requests.post(settings.AUDIT_CALLBACK_URL, json=data)
-            error = resp.status_code != 200
-        except Exception: # noqa
-            error = True
-
-        if error:
-            # TODO 考虑失败情况，最终失败落入 -> 指定文件中 audits_callback_error.json
-            pass
+        for i in range(3):
+            try:
+                data['job_id'] = JobUtil(data['user_id']).get_job()
+                resp = requests.post(settings.AUDIT_CALLBACK_URL, json=data)
+                error = resp.status_code != 200
+            except Exception: # noqa
+                error = True
+            if error:
+                time.sleep(5)
+            else:
+                break
+        else:
+            with open(FAILED_REQUEST_FILE, 'a') as f:
+                f.write(str(data))
