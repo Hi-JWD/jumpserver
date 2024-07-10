688bae160189b83bf501fa90c4f046329ffbbfc7
jiangweidong <1053570670@qq.com>
2024-07-10 11:52:10 +0800

    perf: parse script

--- apps/behemoth/libs/parser/handle.py
+++ apps/behemoth/libs/parser/handle.py
@@ -6,11 +6,16 @@ from typing import List
 
 from django.conf import settings
 
+from common.utils import get_logger
+
 
 if platform.system() == "Darwin":
     so_dir = 'parse_darwin.so'
 else:
     so_dir = 'parse_linux.so'
+
+
+logger = get_logger(__file__)
 
 lib = ctypes.CDLL(os.path.join(settings.APPS_DIR, 'behemoth', 'libs', 'parser', so_dir))
 lib.Parse.argtypes = [ctypes.c_char_p] # noqa
@@ -20,6 +25,8 @@ lib.Parse.restype = ctypes.c_char_p
 def parse_sql(sql: str) -> List[str]:
     try:
         result = lib.Parse(sql.encode('utf-8'))
-    except BaseException: # noqa
+        result.decode('utf-8').split('\n')
+    except Exception as e: # noqa
+        logger.error('Parse sql error: %s' % e)
         result = ''
-    return result.decode('utf-8').split('\n')
+    return result
